2620abc76cc92dafda0fb5bd796901730eae488c
funsharebacode <37871332+funsharebacode@users.noreply.github.com>
2023-08-11 19:52:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 import requests
 import json
-import time
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 
@@ -16,17 +17,20 @@ def user():
 
 @app.route('/data')
 def get_web_data():
-    t = time.localtime()
-    date = str(t.tm_year) + "-" + str(t.tm_mon).zfill(2) + "-" + str(t.tm_mday).zfill(2)
-    res = requests.get("https://stockapi.com.cn/v1/base/dragonTiger?date="+date)
-    res_text = res.text
-    stocks = json.loads(res_text)
+    date = datetime.now(pytz.timezone('Asia/Shanghai'))
+    try:
+        res = requests.get("https://stockapi.com.cn/v1/base/dragonTiger?date="+date.date())
+        res_text = res.text
+        stocks = json.loads(res_text)
+    
+        content = set()
+    
+        for stock in stocks["data"]:
+            content.add(stock["name"])
 
-    content = set()
+        # for r in content:
+        #     print(r)
+        return "<h3>"+date+"龙虎榜数据：</h3>"+str(content)
 
-    for stock in stocks["data"]:
-        content.add(stock["name"])
-
-    # for r in content:
-    #     print(r)
-    return "<h3>"+date+"龙虎榜数据：</h3>"+str(content)
+      except Exception as e:
+          return '暂无相关数据...'
