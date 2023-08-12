bdbbe1d73f370cd284462c7d52fca507ebdce569
funsharebacode <37871332+funsharebacode@users.noreply.github.com>
2023-08-12 17:10:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import json
 from datetime import datetime
 import pytz
+import pywencai
 
 app = Flask(__name__)
 
@@ -12,7 +13,24 @@ def hello_world():
 
 @app.route('/user')
 def user():
-    return 'user...'
+    # 引入 pywencai 库
+    res = pywencai.get(query='当前涨幅',find=['600519', '000010'])
+    # 获取列名 -> 涨跌幅:前复权[20230811]
+    rate = res.columns[3]
+    # 获取前几行数据
+    stock_detail = res[['股票简称',rate]][:3]
+    # 创建字符串 stock_str = {'stocks':['贵州茅台','-2.18%'],['美丽生态','-2.98%'],['科净源','113.33']}
+    stock_str = "{'stocks':["
+    for index,row in stock_detail.iterrows():
+        stock_str += '{'
+        stock_str += '"{}":"{}","{}":"{}%"'.format("name",stock_detail.iloc[index]['股票简称'],"rate",stock_detail.iloc[index][rate][:-6])
+        stock_str += '},'
+    stock_str += "]}"
+    stocks = stock_str.replace(",]}","]}")
+    # 生成json api
+    stock_api = json.dumps(stocks,ensure_ascii=False)
+    
+    return stock_api
 
 @app.route('/data')
 def get_web_data():
