ca8d11df24bc2bbd7b4f18f4637f20a1cf082f18
funsharebacode <37871332+funsharebacode@users.noreply.github.com>
2023-09-05 22:42:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def user():
 
     # 获取彩云天气接口
     # 获取用户请求时提供的位置信息
-    pos = request.args.get('pos', "北 90.000000'东 31.171717'")
+    pos = request.args.get('pos', "北 90.000000­°东 31.171717­°")
     p = pos.split("­°")
     lastpos = ''
     lastpos += p[0][-10:].lstrip() + "," + p[1][-10:].lstrip()
