a86ad2f7d27376fc0e50ecea580bd7c312cad7a2
funsharebacode <37871332+funsharebacode@users.noreply.github.com>
2023-09-10 22:56:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,89 +58,100 @@ def user():
             rate = (float(stocks[3]) - float(stocks[2])) / float(stocks[2]) * 100
         else:
             rate = 0
+
+
+
         info_api["stocks"].append({"code": user_stocks[index], "name": stocks[0][1:], "rate": ('%.2f' % rate) + '%'})
 
     # 获取彩云天气接口
     # 获取用户请求时提供的位置信息
     pos = request.args.get('pos', "90.000000,31.171717")
     weathercode = request.args.get('weathercode', "TAkhjf8d1nlSlspN")
-    
+
     url_a = "https://api.caiyunapp.com/v2.6/" + weathercode + "/" + pos + "/weather?alert=true&dailysteps=1&hourlysteps=24"
     url_b = "https://api.caiyunapp.com/v2.6/" + weathercode + "/" + pos + "/realtime?alert=true"
 
     caiyun = json.loads(requests.get(url_a).text)
     area = requests.get(url_b).text
 
-    # 天气预报地区
-    # city = caiyun["result"]["alert"]["adcodes"][0]["name"] + "|" + caiyun["result"]["alert"]["adcodes"][2]["name"]
-    city = caiyun["result"]["alert"]["adcodes"][2]["name"]
-    # 天气现象
-    sky_con = {"CLEAR_DAY": "晴（白天）", "CLEAR_NIGHT": "晴（夜间）", "PARTLY_CLOUDY_DAY": "多云（白天）",
-               "PARTLY_CLOUDY_NIGHT": "多云（夜间）",
-               "CLOUDY": "阴", "LIGHT_HAZE": "轻度雾霾", "MODERATE_HAZE": "中度雾霾", "HEAVY_HAZE": "重度雾霾",
-               "LIGHT_RAIN": "小雨",
-               "MODERATE_RAIN": "中雨", "HEAVY_RAIN": "大雨", "STORM_RAIN": "暴雨", "FOG": "雾", "LIGHT_SNOW": "小雪",
-               "MODERATE_SNOW": "中雪",
-               "HEAVY_SNOW": "大雪", "STORM_SNOW": "暴雪", "DUST": "浮尘", "SAND": "沙尘", "WIND": "大风"}
-    # 气温（地表2米气温）
-    temperature = int(caiyun["result"]["realtime"]["temperature"])
-    # 天气现象
-    skycon = sky_con[caiyun["result"]["realtime"]["skycon"]]
-    # 气压
-    pressure = '{}'.format(int(caiyun["result"]["realtime"]["pressure"]) / 100)
-    # wind
-    # 风速
-    wind_speed = '{}'.format(caiyun["result"]["realtime"]["wind"]["speed"]) + '米'
-    # 风向
-    wind_direction = int(caiyun["result"]["daily"]["wind_08h_20h"][0]["avg"]["direction"])
-    if wind_direction * 100 in range(1126, 3375):
-        wind_direct = '北-东北风'
-    elif wind_direction * 100 in range(3376, 5625):
-        wind_direct = '东北风'
-    elif wind_direction * 100 in range(5626 - 7875):
-        wind_direct = '东-东北风'
-    elif wind_direction * 100 in range(7876, 10125):
-        wind_direct = '东风'
-    elif wind_direction * 100 in range(10126, 12375):
-        wind_direct = '东-东南风'
-    elif wind_direction * 100 in range(12376, 14625):
-        wind_direct = '东南风'
-    elif wind_direction * 100 in range(14626, 16875):
-        wind_direct = '南-东南风'
-    elif wind_direction * 100 in range(16876, 19125):
-        wind_direct = '南风'
-    elif wind_direction * 100 in range(19126, 21375):
-        wind_direct = '南-西南风'
-    elif wind_direction * 100 in range(21376, 23625):
-        wind_direct = '西南风'
-    elif wind_direction * 100 in range(23626, 25875):
-        wind_direct = '西-西南风'
-    elif wind_direction * 100 in range(25876, 28125):
-        wind_direct = '西风'
-    elif wind_direction * 100 in range(28126, 30375):
-        wind_direct = '西-西北风'
-    elif wind_direction * 100 in range(30376, 32625):
-        wind_direct = '西北风'
-    elif wind_direction * 100 in range(32626, 34875):
-        wind_direct = '北-西北风'
-    else:
-        wind_direct = '北风'
 
-    # 天气质量
-    description_a = caiyun["result"]["realtime"]["air_quality"]["description"]["chn"]
-    description_b = caiyun["result"]["realtime"]["air_quality"]["description"]["usa"]
-    # 最高气温
-    max_temp = int(caiyun["result"]["daily"]["temperature"][0]["max"])
-    # 最低气温
-    min_temp = int(caiyun["result"]["daily"]["temperature"][0]["min"])
-    # 未来两小时降水情况
-    forecast_keypoint = caiyun["result"]["forecast_keypoint"]
+    try:
+        # 天气预报地区
+        # city = caiyun["result"]["alert"]["adcodes"][0]["name"] + "|" + caiyun["result"]["alert"]["adcodes"][2]["name"]
+        city = caiyun["result"]["alert"]["adcodes"][2]["name"]
+        # 天气现象
+        sky_con = {"CLEAR_DAY": "晴（白天）", "CLEAR_NIGHT": "晴（夜间）", "PARTLY_CLOUDY_DAY": "多云（白天）",
+                   "PARTLY_CLOUDY_NIGHT": "多云（夜间）",
+                   "CLOUDY": "阴", "LIGHT_HAZE": "轻度雾霾", "MODERATE_HAZE": "中度雾霾", "HEAVY_HAZE": "重度雾霾",
+                   "LIGHT_RAIN": "小雨",
+                   "MODERATE_RAIN": "中雨", "HEAVY_RAIN": "大雨", "STORM_RAIN": "暴雨", "FOG": "雾",
+                   "LIGHT_SNOW": "小雪",
+                   "MODERATE_SNOW": "中雪",
+                   "HEAVY_SNOW": "大雪", "STORM_SNOW": "暴雪", "DUST": "浮尘", "SAND": "沙尘", "WIND": "大风"}
+        # 气温（地表2米气温）
+        temperature = int(caiyun["result"]["realtime"]["temperature"])
+        # 天气现象
+        skycon = sky_con[caiyun["result"]["realtime"]["skycon"]]
+        # 气压
+        pressure = '{}'.format(int(caiyun["result"]["realtime"]["pressure"]) / 100)
+        # wind
+        # 风速
+        wind_speed = '{}'.format(caiyun["result"]["realtime"]["wind"]["speed"]) + '米'
+        # 风向
+        wind_direction = int(caiyun["result"]["daily"]["wind_08h_20h"][0]["avg"]["direction"])
+        if wind_direction * 100 in range(1126, 3375):
+            wind_direct = '北-东北风'
+        elif wind_direction * 100 in range(3376, 5625):
+            wind_direct = '东北风'
+        elif wind_direction * 100 in range(5626 - 7875):
+            wind_direct = '东-东北风'
+        elif wind_direction * 100 in range(7876, 10125):
+            wind_direct = '东风'
+        elif wind_direction * 100 in range(10126, 12375):
+            wind_direct = '东-东南风'
+        elif wind_direction * 100 in range(12376, 14625):
+            wind_direct = '东南风'
+        elif wind_direction * 100 in range(14626, 16875):
+            wind_direct = '南-东南风'
+        elif wind_direction * 100 in range(16876, 19125):
+            wind_direct = '南风'
+        elif wind_direction * 100 in range(19126, 21375):
+            wind_direct = '南-西南风'
+        elif wind_direction * 100 in range(21376, 23625):
+            wind_direct = '西南风'
+        elif wind_direction * 100 in range(23626, 25875):
+            wind_direct = '西-西南风'
+        elif wind_direction * 100 in range(25876, 28125):
+            wind_direct = '西风'
+        elif wind_direction * 100 in range(28126, 30375):
+            wind_direct = '西-西北风'
+        elif wind_direction * 100 in range(30376, 32625):
+            wind_direct = '西北风'
+        elif wind_direction * 100 in range(32626, 34875):
+            wind_direct = '北-西北风'
+        else:
+            wind_direct = '北风'
 
-    info_api["temperature"] = {"temperature": temperature, "skycon": skycon, "pressure": pressure,
-                               "wind_speed": wind_speed, "wind_direct": wind_direct,
-                               "description_a": description_a, "description_b": description_b, "max_temp": max_temp,
-                               "min_temp": min_temp, "forecast_keypoint": forecast_keypoint,
-                               "city": city}
+        # 天气质量
+        description_a = caiyun["result"]["realtime"]["air_quality"]["description"]["chn"]
+        description_b = caiyun["result"]["realtime"]["air_quality"]["description"]["usa"]
+        # 最高气温
+        max_temp = int(caiyun["result"]["daily"]["temperature"][0]["max"])
+        # 最低气温
+        min_temp = int(caiyun["result"]["daily"]["temperature"][0]["min"])
+        # 未来两小时降水情况
+        forecast_keypoint = caiyun["result"]["forecast_keypoint"]
+
+        info_api["temperature"] = {"temperature": temperature, "skycon": skycon, "pressure": pressure,
+                                   "wind_speed": wind_speed, "wind_direct": wind_direct,
+                                   "description_a": description_a, "description_b": description_b, "max_temp": max_temp,
+                                   "min_temp": min_temp, "forecast_keypoint": forecast_keypoint,
+                                   "city": city}
+    except KeyError as e:
+        print(e)
+
+
+
 
     return json.dumps(info_api, ensure_ascii=False)
     
